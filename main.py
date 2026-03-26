8be872e10782d047d96bf47e879b604350c837ba
Ilya <ilyakitoy@gmail.com>
2026-03-26 17:02:25 +0300

    structuring code and adding new functions

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import logging 
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 from config_reader import config
+from keyboards.keyboards import main_menu, goal_keyboard, gender_keyboard, activity_keyboard
+
+
 
 bot = Bot(token=config.bot_token.get_secret_value())
 
-dp = Dispatcher()
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer('💪 Добро пожаловать в FitCount! \n Привет! Я — твой персональный счетовод в мире питания. \n Моя задача — сделать подсчет калорий таким же простым, как и шагомер считает твои шаги.\n FitCount умеет: \n 🧮 Точный счет. Быстрый поиск калорий и БЖУ по названию продукта. \n 📊 Контроль прогресса. Веди дневник питания и следи, укладываешься ли ты в норму. \n 💪 Контроль тренеровок. Записывай свои тренервоки и следи за прогрессом. \n Давай настроим профиль, введи свои данные 👇 ')
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
     
 async def main():
     await dp.start_polling(bot)
-    
+
+
 if __name__ == "__main__":
     asyncio.run(main())
-    
