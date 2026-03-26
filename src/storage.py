d562e2cbfde281ef3219082d7328c3a75ca1a42b
Ilya <ilyakitoy@gmail.com>
2026-03-26 17:07:20 +0300

    Add user data management functions in storage.py
    Implement functions to load, save, and retrieve user data from a JSON file.

---
+++ src/storage.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+FILE_PATH = "data/users.json"
+
+
+# загрузка всех пользователей
+def load_users():
+    if not os.path.exists(FILE_PATH):
+        return {}
+
+    with open(FILE_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+# сохранение всех пользователей
+def save_users(users_data):
+    with open(FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump(users_data, f, ensure_ascii=False, indent=4)
+
+
+# сохранить одного пользователя
+def save_user(user_id: int, data: dict):
+    users = load_users()
+    users[str(user_id)] = data
+    save_users(users)
+
+
+# получить пользователя
+def get_user(user_id: int):
+    users = load_users()
+    return users.get(str(user_id))
