cd0fa36b366e323e1fa3cb0c111e6ee5c4776b8a
Ilya <ilyakitoy@gmail.com>
2026-04-11 23:13:17 +0300

    Added new data for input

--- src/storage.py
+++ src/storage.py
@@ -20,21 +20,38 @@ def load_users():
     except json.JSONDecodeError:
         return {}
 
-
-# сохранение всех пользователей
 def save_users(users_data):
     with open(FILE_PATH, "w", encoding="utf-8") as f:
         json.dump(users_data, f, ensure_ascii=False, indent=4)
 
-
-# сохранить одного пользователя
 def save_user(user_id: int, data: dict):
     users = load_users()
     users[str(user_id)] = data
     save_users(users)
 
-
-# получить пользователя
 def get_user(user_id: int):
     users = load_users()
     return users.get(str(user_id))
+
+def save_all_users(users):
+    with open("src/users.json", "w") as f:
+        json.dump(users, f, indent=4)
+        
+def update_eaten(user_id, cals, prot, fat, carbs):
+    users = load_users()
+
+    user_id = str(user_id)
+
+    if user_id not in users:
+        return
+
+    user = users[user_id]
+
+    user["eaten_calories"] = user.get("eaten_calories", 0) + cals
+    user["eaten_protein"] = user.get("eaten_protein", 0) + prot
+    user["eaten_fat"] = user.get("eaten_fat", 0) + fat
+    user["eaten_carbs"] = user.get("eaten_carbs", 0) + carbs
+
+    users[user_id] = user
+
+    save_all_users(users)
